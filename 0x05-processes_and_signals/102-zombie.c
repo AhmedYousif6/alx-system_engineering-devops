fb7c92ae2f736ece79e94996396c5478f5f45214
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-01-26 14:57:19 -0800

    modify

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -24,19 +24,17 @@ int infinite_while(void)
 int main(void)
 {
 	pid_t pid;
-	char i = 0;
+	int i = 0;
 
 	while (i < 5)
 	{
 		pid = fork();
-		if (pid > 0)
+		if (pid == 0)
 		{
-			printf("Zombie process created, PID: %d\n");
+			printf("Zombie process created, PID: %d\n", getpid());
 			sleep(1);
-			i++;
 		}
-		else
-			exit(0);
+		i++;
 	}
 	infinite_while();
 	return (0);
