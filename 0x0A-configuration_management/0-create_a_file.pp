48c620f24cb5f45b36b79f5b8096a77cc37cc378
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-04-22 14:59:23 -0700

    j

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# this manifest creates file at tmp
+# Creates a file
 file { '/tmp/school':
-    ensure  => 'file',
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',    
-    group   => 'www-data',
-    content => 'I Love Puppet',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
