cc8e50858685cd6028fa6b6d6797ceb62f1c8b5a
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-04-22 14:52:39 -0700

    modify

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,6 +3,7 @@ file { '/tmp/school':
     ensure  => 'file',
     owner   => 'www-data',
     group   => 'www-data',
+    path    => '/tmp/school',
     mode    => '0744',
     content => 'I Love Puppet',
 }
